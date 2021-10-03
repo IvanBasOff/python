ae6d274436743f624590f18c5e467e9b221293cd
IvanBasOff <86849597+IvanBasOff@users.noreply.github.com>
2021-10-03 19:16:24 +0300

    pythonProject01 added
    pythonProject01 added. Django learning

---
+++ pythonProject01/taskmanager/main/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+# Create your views here.
+def index(request):
+    #tasks = Task.objects.all()
+    #tasks = Task.objects.order_by('id')
+    tasks = Task.objects.order_by('-id')
+    #tasks = Task.objects.order_by('-id')[:5]
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма заполнена неверно'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
